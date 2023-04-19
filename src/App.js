0921f53d0c235cb88fb5860cefdb1fe161171ac5
yıldız <mesel4461@gmail.com>
2023-04-19 11:18:49 +0300

    as

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-full w-full flex item-center justify-center">
+      <div className="w-[380px] h-[581px]">
+        <img
+          className="w-full h-full"
+          src="https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones-2x.png?__makehaste_cache_breaker=73SVAexZgBW"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
