21ce64d00802ed99b2a0e9286cf22fd7a5848fc8
yıldız <mesel4461@gmail.com>
2023-04-19 12:00:33 +0300

    as

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import photo1 from "./assets/photo1.png";
+import photo2 from "./assets/photo2.png";
+import photo3 from "./assets/photo3.png";
+import photo4 from "./assets/photo4.png";
 function App() {
   return (
-    <div className="h-full w-full flex item-center justify-center">
-      <div className="w-[380px] h-[581px]">
-        <img
-          className="w-full h-full"
-          src="https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones-2x.png?__makehaste_cache_breaker=73SVAexZgBW"
-          alt=""
-        />
+    <div className="h-full w-full flex items-center justify-center">
+      <div className="w-[380px] h-[581px] relative bg-logo-pattern bg-[length:468.32px_634.15px] bg-[top_left_-46px]">
+        <div className="w-[250px] h-[538px] absolute top-[27px] right-[18px]">
+          <img className="w-full h-full" src={photo1} alt="" />
+          <img className="w-full h-full" src={photo2} alt="" />
+          <img className="w-full h-full" src={photo3} alt="" />
+          <img className="w-full h-full" src={photo4} alt="" />
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "logo-pattern":
+          "url(https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones-2x.png?__makehaste_cache_breaker=73SVAexZgBW)",
+      },
+    },
   },
   plugins: [],
 };
