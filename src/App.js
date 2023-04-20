864ad08e2288aa918aa0d1dfdfcf265295aa8a24
yıldız <mesel4461@gmail.com>
2023-04-21 02:25:25 +0300

    as

--- src/App.js
+++ src/App.js
@@ -1,108 +1,126 @@
-import photo1 from "./assets/photo1.png";
-import photo2 from "./assets/photo2.png";
-import photo3 from "./assets/photo3.png";
-import photo4 from "./assets/photo4.png";
-import { useRef, useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Input from "./components/Input";
+import { AiFillFacebook } from "react-icons/ai";
 
 function App() {
+  const ref = useRef();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const enable = username && password;
-  const ref = useRef();
+
   useEffect(() => {
-    let images = ref.current.querySelectorAll("img");
-    let total = images.length,
-      count = 0;
+    let images = ref.current.querySelectorAll("img"),
+      total = images.length,
+      current = 0;
     const imageSlider = () => {
-      if (count > 0) {
-        images[count - 1].classList.add("opacity-0");
+      if (current > 0) {
+        images[current - 1].classList.add("opacity-0");
       } else {
         images[total - 1].classList.add("opacity-0");
       }
-      images[count].classList.remove("opacity-0");
-      if (count === total - 1) {
-        count = 0;
+      images[current].classList.remove("opacity-0");
+      if (current === total - 1) {
+        current = 0;
       } else {
-        count += 1;
+        current += 1;
       }
     };
-    let interval = setInterval(imageSlider, 3000);
     imageSlider();
+    let interval = setInterval(imageSlider, 3000);
     return () => {
       clearInterval(interval);
     };
   }, [ref]);
 
   return (
-    <div className="h-full w-full flex items-center justify-center gap-x-8">
-      <div className="w-[380px] h-[581px] relative bg-logo-pattern bg-[length:468.32px_634.15px] bg-[top_left_-46px]">
+    <div className="h-full w-full flex flex-wrap overflow-auto items-center gap-x-8 justify-center">
+      <div className="hidden md:block w-[380px] h-[581px] bg-logo-pattern relative bg-[length:468.32px_634.15px] bg-[top_left_-46px]">
         <div
           className="w-[250px] h-[538px] absolute top-[27px] right-[18px]"
           ref={ref}
         >
           <img
-            className="w-full h-full absolute left-0 top-0 opacity-0 transition-opacity duration-1000 ease-in"
-            src={photo1}
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
+            src="https://www.instagram.com/static/images/homepage/screenshots/screenshot1-2x.png/cfd999368de3.png"
             alt=""
           />
           <img
-            className="w-full h-full absolute left-0 top-0 opacity-0 transition-opacity duration-1000 ease-in"
-            src={photo2}
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
+            src="https://www.instagram.com/static/images/homepage/screenshots/screenshot2-2x.png/80b8aebdea57.png"
             alt=""
           />
           <img
-            className="w-full h-full absolute left-0 top-0 opacity-0 transition-opacity duration-1000 ease-in"
-            src={photo3}
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
+            src="https://www.instagram.com/static/images/homepage/screenshots/screenshot3-2x.png/fe2540684ab2.png"
             alt=""
           />
           <img
-            className="w-full h-full absolute left-0 top-0 opacity-0 transition-opacity duration-1000 ease-in"
-            src={photo4}
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
+            src="https://www.instagram.com/static/images/homepage/screenshots/screenshot4-2x.png/8e9224a71939.png"
             alt=""
           />
         </div>
       </div>
-      <div className="w-[350px] bg-white border px-[40px] pt-8 pb-2">
-        <a href="#" className="flex justify-center mb-8">
-          <img
-            className="h-[100px]"
-            src="https://1000logos.net/wp-content/uploads/2017/02/Logo-Instagram.png"
-            alt=""
-          />
-        </a>
 
-        <form className="grid gap-y-1.5">
-          <Input
-            type="text"
-            value={username}
-            label="Phone number, username or email"
-            onChange={(e) => setUsername(e.target.value)}
-          />
+      <div className="w-[350px] grid gap-y-3">
+        <div className="bg-white border px-[40px] pt-10 pb-6">
+          <a href="#" className="flex justify-center mb-8">
+            <img
+              className="h-[51px]"
+              src="https://www.instagram.com/static/images/web/logged_out_wordmark-2x.png/d2529dbef8ed.png"
+              alt=""
+            />
+          </a>
+          <form className="grid gap-y-1.5">
+            <Input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              label="Phone number, username or email"
+            />
+            <Input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              label="Password"
+            />
+            <button
+              type="submit"
+              disabled={!enable}
+              className="h-[30px] mt-1 rounded bg-brand font-medium text-white text-sm disabled:opacity-50"
+            >
+              Log In
+            </button>
+            <div className="flex items-center my-2.5 mb-3.5">
+              <div className="h-px bg-gray-300 flex-1" />
+              <span className="px-4 text-[13px] text-gray-500 font-semibold">
+                OR
+              </span>
+              <div className="h-px bg-gray-300 flex-1" />
+            </div>
+            <a
+              href="#"
+              className="flex justify-center mb-2.5 items-center gap-x-2 text-sm font-semibold text-facebook"
+            >
+              <AiFillFacebook size={20} />
+              Log in with Facebook
+            </a>
+            <a
+              href="#"
+              className="text-xs flex items-center justify-center text-link"
+            >
+              Forgot password?
+            </a>
+          </form>
+        </div>
 
-          <Input
-            type="password"
-            value={password}
-            label="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-
-          <button
-            disabled={!enable}
-            type="submit"
-            className="h-[30px] rounded bg-brand text-white text-sm disabled:opacity-50"
-          >
-            Log In
-          </button>
-          <div className="flex items-center">
-            <div className="h-px bg-gray-300 flex-1" />
-            <span className="px-4 text-[13px] text-gray-500 font-semibold">
-              OR
-            </span>
-            <div className="h-px bg-gray-300 flex-1" />
-          </div>
-        </form>
+        <div className="bg-white border p-4 text-sm text-center">
+          Don't have an account?{" "}
+          <a href="#" className="font-semibold text-brand">
+            Sign up
+          </a>
+        </div>
       </div>
     </div>
   );
