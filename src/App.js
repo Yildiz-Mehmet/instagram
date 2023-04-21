97016e2912a8ca4d29a5f4f1dc593bfb7b9a6a1f
yıldız <mesel4461@gmail.com>
2023-04-21 05:23:14 +0300

    as

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   }, [ref]);
 
   return (
-    <div className="w-full h-full flex items-center justify-center">
+    <div className="w-full h-full flex items-center justify-center gap-x-8">
       <div className="w-[380px] h-[581px] bg-logo-pattern bg-[length:468.32px_634.15px] bg-[top_left_-46px] relative ">
         <div
           className="w-[250px] h-[538px] absolute top-[27px] right-[18px]"
@@ -57,13 +57,44 @@ function App() {
           />
         </div>
       </div>
-      <div className="w-[350px] bg-white border">
-        <img
-          className="h-[51px] "
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png"
-          alt=""
-        />
-        <form></form>
+      <div className="w-[350px] bg-white border p-[40px] pt-10 pb-2">
+        <a href="#" className="flex items-center justify-center mb-8">
+          <img
+            className="h-[51px] "
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png"
+            alt=""
+          />
+        </a>
+
+        <form className="grid gap-y-1.5">
+          <label className="block relative">
+            <input
+              required={true}
+              type="text"
+              className="bg-zing-50 border text-xs w-full h-[38px] px-2 rounded-sm outline-none focus:border-gray-400 valid:pt-[10px] peer"
+            />
+            <small className="absolute top-1/2 left-[8px] text-xs cursor-text pointer-events-none text-gray-500 -translate-y-1/2 transition-all peer-valid:text-[10px] peer-valid:top-2.5">
+              For number, username or email
+            </small>
+          </label>
+          <label className="block relative">
+            <input
+              required={true}
+              type="password"
+              className="bg-zing-50 border text-xs w-full h-[38px] px-2 rounded-sm outline-none focus:border-gray-400 valid:pt-[10px] peer"
+            />
+            <small className="absolute top-1/2 left-[8px] text-xs cursor-text pointer-events-none text-gray-500 -translate-y-1/2 transition-all peer-valid:text-[10px] peer-valid:top-2.5">
+              Password
+            </small>
+          </label>
+          <button
+            disabled={true}
+            type="submit"
+            className="h-[30px] rounded bg-brand font-medium text-white text-sm disabled:opacity-50"
+          >
+            Log In
+          </button>
+        </form>
       </div>
     </div>
   );
